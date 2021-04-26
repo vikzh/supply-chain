c2ebe39b95cee3518939325f322c088b702ccd88
vikzh <vikt.anmail@gmail.com>
2021-04-26 16:06:47 +0300

    item manager only owner rights added

--- contracts/ItemManager.sol
+++ contracts/ItemManager.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.8;
 
 import "./Item.sol";
+import "./Ownable.sol";
 
-contract ItemManager {
+contract ItemManager is Ownable {
     enum SupplyChainSteps{Created, Paid, Delivered}
 
     struct S_Item {
@@ -16,7 +17,7 @@ contract ItemManager {
 
     event SupplyChainStep(uint _itemIndex, uint _step, address _address);
 
-    function createItem(string memory _identifier, uint _priceInWei) public {
+    function createItem(string memory _identifier, uint _priceInWei) public onlyOwner{
         Item item = new Item(this, _priceInWei, index);
         items[index]._item = item;
         items[index]._identifier = _identifier;
@@ -34,7 +35,7 @@ contract ItemManager {
         emit SupplyChainStep(index, uint(items[index]._step), address(item));
     }
 
-    function triggerDelivery(uint _index) public {
+    function triggerDelivery(uint _index) public onlyOwner{
         require(items[_index]._step == SupplyChainSteps.Paid, "Item is in wrong supply chain");
         items[_index]._step = SupplyChainSteps.Delivered;
         emit SupplyChainStep(_index, uint(items[_index]._step), address(items[_index]._item));
