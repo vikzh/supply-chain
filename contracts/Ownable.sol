e1f62504e6a7df7f0784f5a55295e1a80f07d1ec
vikzh <vikt.anmail@gmail.com>
2021-04-26 15:46:31 +0300

    ownable contract added

---
+++ contracts/Ownable.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8;
+
+contract Ownable {
+    address public _owner;
+    
+    constructor() {
+        _owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(isOwner(), "Caller is not the owner");
+        _;
+    }
+
+    function isOwner() public view returns (bool){
+        return msg.sender == _owner;
+    }
+}
